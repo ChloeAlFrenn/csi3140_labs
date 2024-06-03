936828ab228ac9fa6183dafa2cbec7782eadd474
Chloe Al-Frenn <chloealfrenn@hotmail.com>
2024-06-03 15:06:45 -0400

    Make it so there is only one fruit instead of multiple

--- lab03/pacman.js
+++ lab03/pacman.js
@@ -13,45 +13,40 @@ function createGame(n) {
   } while (ghostIndex === pacmanIndex);
   gameBoard[ghostIndex] = "^";
 
-  // Generate a random number of fruits between 1 and n-2
-  let numFruits = Math.floor(1 + Math.random() * (n - 2));
-
-  // Place the fruits
-  for (let i = 0; i < numFruits; i++) {
-    let fruitIndex;
-    do {
-      fruitIndex = Math.floor(Math.random() * n);
-    } while (
-      fruitIndex === pacmanIndex ||
-      fruitIndex === ghostIndex ||
-      gameBoard[fruitIndex] === "@"
-    );
-    gameBoard[fruitIndex] = "@";
-  }
+  // Place the Fruit
+  let fruitIndex;
+  do {
+    fruitIndex = Math.floor(Math.random() * n);
+  } while (
+    fruitIndex === pacmanIndex ||
+    fruitIndex === ghostIndex ||
+    gameBoard[fruitIndex] === "@"
+  );
+  gameBoard[fruitIndex] = "@";
 
   return gameBoard;
 }
 
 function getPacmanIndex(game) {
-    return game.indexOf('C');
+  return game.indexOf("C");
 }
 
 function moveLeft(game) {
-    let pacmanIndex = getPacmanIndex(game);
-    if (pacmanIndex > 0) {
-        game[pacmanIndex] = '.';
-        game[pacmanIndex - 1] = 'C';
-    }
-    return game;
+  let pacmanIndex = getPacmanIndex(game);
+  if (pacmanIndex > 0) {
+    game[pacmanIndex] = ".";
+    game[pacmanIndex - 1] = "C";
+  }
+  return game;
 }
 
 function moveRight(game) {
-    let pacmanIndex = getPacmanIndex(game);
-    if (pacmanIndex < game.length - 1) {
-        game[pacmanIndex] = '.';
-        game[pacmanIndex + 1] = 'C';
-    }
-    return game;
+  let pacmanIndex = getPacmanIndex(game);
+  if (pacmanIndex < game.length - 1) {
+    game[pacmanIndex] = ".";
+    game[pacmanIndex + 1] = "C";
+  }
+  return game;
 }
 
 let gameBoard = createGame(10);
