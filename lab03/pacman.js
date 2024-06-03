746ce514e46a7de2d1807bfa70113b81588d9c70
Chloe Al-Frenn <chloealfrenn@hotmail.com>
2024-06-03 15:05:39 -0400

    Added the option for the pacman to move left and right

--- lab03/pacman.js
+++ lab03/pacman.js
@@ -11,7 +11,7 @@ function createGame(n) {
   do {
     ghostIndex = Math.floor(Math.random() * n);
   } while (ghostIndex === pacmanIndex);
-  gameBoard[ghostIndex] = "^.";
+  gameBoard[ghostIndex] = "^";
 
   // Generate a random number of fruits between 1 and n-2
   let numFruits = Math.floor(1 + Math.random() * (n - 2));
@@ -32,4 +32,33 @@ function createGame(n) {
   return gameBoard;
 }
 
-console.log(createGame(10));
+function getPacmanIndex(game) {
+    return game.indexOf('C');
+}
+
+function moveLeft(game) {
+    let pacmanIndex = getPacmanIndex(game);
+    if (pacmanIndex > 0) {
+        game[pacmanIndex] = '.';
+        game[pacmanIndex - 1] = 'C';
+    }
+    return game;
+}
+
+function moveRight(game) {
+    let pacmanIndex = getPacmanIndex(game);
+    if (pacmanIndex < game.length - 1) {
+        game[pacmanIndex] = '.';
+        game[pacmanIndex + 1] = 'C';
+    }
+    return game;
+}
+
+let gameBoard = createGame(10);
+console.log("Initial Board:", gameBoard);
+
+gameBoard = moveLeft(gameBoard);
+console.log("After moveLeft:", gameBoard);
+
+gameBoard = moveRight(gameBoard);
+console.log("After moveRight:", gameBoard);
